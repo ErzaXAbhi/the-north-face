64e6cdb7dd01f8b92cd611efcc47e7cd63a3928b
Abhijit Parad <ErzaXAbhi>
2023-09-11 19:11:04 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const everest = document.querySelector(".everest");
+const title = document.querySelector("h1");
+const heading = document.querySelector(".heading");
+
+gsap.to(everest, {
+  y: 200,
+  scrollTrigger: {
+    trigger: "everest",
+    scrub: true,
+  },
+});
+
+gsap.from(heading, {
+  y: -850,
+  autoAlpha: 0,
+  delay: 0.5,
+  scrollTrigger: {
+    trigger: "clouds",
+    scrub: true,
+  },
+});
+
+gsap.to(title, {
+  y: 500,
+  scrollTrigger: {
+    trigger: "everest",
+    scrub: true,
+  },
+});
